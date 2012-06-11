6be62b81acf3fa0b8559268c3c3d232d01ef72e4
Jonathan Kim <jkimbo@gmail.com>
2012-06-11 19:06:58 +0100

    Added timer loop and reload cache on each loop

--- app.js
+++ app.js
@@ -3,7 +3,54 @@
  *
  * Generates data for coaches and posts to server
  */
+var config = require('./config')
+  , request = require('request');
 
-var config = require('./config');
+var setVariableInterval = function(callbackFunc, timing) {
+  var variableInterval = {
+    interval: timing,
+    callback: callbackFunc,
+    stopped: false,
+    runLoop: function() {
+      if (variableInterval.stopped) return;
+      var result = variableInterval.callback.call(variableInterval);
+      if (typeof result == 'number') {
+        if (result === 0) return;
+        variableInterval.interval = result;
+      }
+      variableInterval.loop();
+    },
+    stop: function() {
+      this.stopped = true;
+      clearTimeout(this.timeout);
+    },
+    start: function() {
+      this.stopped = false;
+      return this.loop();
+    },
+    loop: function() {
+      this.timeout = setTimeout(this.runLoop, this.interval);
+      return this;
+    }
+  };
+  return variableInterval.start();
+};
 
+/*
+ * Publish loop
+ */
+var timer = setVariableInterval(function() {
+  delete require.cache[require.resolve('./config')]; // delete config cache
+  config = require('./config'); // reload config
+  timer.interval = config.duration; // change timer duration
+  request.post({
+    url: config.url+'/coach',
+    json: true,
+    body: {
+      location: 'test'
+    }
+  }, function(e, r, body) {
+    console.log(body);
+  });
+}, config.duration);
 
