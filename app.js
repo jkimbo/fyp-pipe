7fa2a7e7f882903d0d4d4d68185f9becb179051b
Jonathan Kim <jkimbo@gmail.com>
2012-06-13 19:29:09 +0100

    Post to correct url

--- app.js
+++ app.js
@@ -43,11 +43,15 @@ var timer = setVariableInterval(function() {
   delete require.cache[require.resolve('./config')]; // delete config cache
   config = require('./config'); // reload config
   timer.interval = config.duration; // change timer duration
+
+  var time = new Date();
   request.post({
-    url: config.url+'/coach',
+    url: config.url+'/coach/101',
     json: true,
     body: {
-      location: 'test'
+      lat: '51.522522891558964',
+      lng: '-0.1739358901977539',
+      timestamp: time.getTime()
     }
   }, function(e, r, body) {
     console.log(body);
